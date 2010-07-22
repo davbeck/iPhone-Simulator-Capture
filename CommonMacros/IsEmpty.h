873cea0d8f14b2e0dbc2650d01c8e93c07fc045c
David Beck <davbeck@gmail.com>
2010-07-22 15:58:18 -0700

    Added BSD license
    Updated file headers with new liceanse. Added LICENSE file with new LICENSE.

--- CommonMacros/IsEmpty.h
+++ CommonMacros/IsEmpty.h
@@ -1,13 +1,35 @@
 //
 //  IsEmpty.h
 //
-//  Created by David Beck on 12/29/2009.
-//  Copyright 2009 David Beck. All rights reserved.
-//
+//  Copyright (c) 2010, David Beck
+//  All rights reserved.
+//  
+//  Redistribution and use in source and binary forms, with or without modification, 
+//  are permitted provided that the following conditions are met:
+//  
+//  - Redistributions of source code must retain the above copyright notice, 
+//    this list of conditions and the following disclaimer.
+//  - Redistributions in binary form must reproduce the above copyright notice, 
+//    this list of conditions and the following disclaimer in the documentation 
+//    and/or other materials provided with the distribution.
+//  
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+//  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+//  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+//  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+//  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+//  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+//  OF THE POSSIBILITY OF SUCH DAMAGE.
+//  
 
 #import <Foundation/Foundation.h>
 
 //http://www.wilshipley.com/blog/2005/10/pimp-my-code-interlude-free-code.html
+//published under the "DoNotSueMe" license ( http://www.wilshipley.com/blog/2005/10/pimp-my-code-interlude-free-code.html#c112877373361288553 )
+
 static inline BOOL IsEmpty(id thing) {
     return thing == nil
 	|| ([thing respondsToSelector:@selector(length)]
