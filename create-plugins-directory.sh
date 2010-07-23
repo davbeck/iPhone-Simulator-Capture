f8c7407c9dea1ca16f9f7b59fca369c22bf35de8
David Beck <davbeck@gmail.com>
2010-07-22 19:29:44 -0700

    Fixed installer package
    - Added preinstall action to quit the iPhone Simulator (this should really pause
    until the user quits the simulator).
    - Addid preinstall script to create the plugins directory in case the there isn't
    one (without this the directory that is created is not readable by the user)

---
+++ create-plugins-directory.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p /Library/Application\ Support/SIMBL/Plugins
+chown -R root:admin /Library/Application\ Support/SIMBL/Plugins
+chmod -R 775 /Library/Application\ Support/SIMBL/Plugins
